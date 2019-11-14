690609b06b749d1c32c69175720c4abd980d5e8e
Mr. Jonah <jaitch@gmail.com>
2019-11-14 09:52:37 -0600

    Added search

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,27 +1,50 @@
-import React from 'react';
-import { Table, Button, ButtonGroup } from 'reactstrap'
+import React, {useState, useEffect} from 'react';
+import { Table, Button, ButtonGroup, Input } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const UserList = props => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState(props.users);
+
+    console.log(searchResults)
+
+    useEffect(() => {
+        const results = props.users.filter(user =>
+            user.name.toLowerCase()
+            .includes(searchTerm.toLocaleLowerCase()));
+        
+        setSearchResults(results)
+    }, [props, searchTerm])
+
+    const searchHandler = e =>{
+        setSearchTerm(e.target.value);
+    }
+
     return (
         <div>
             <Table>
                 <thead>
                     <tr>
-                        <td>Name</td>
-                        <td>Email</td>
-                        <td>Role</td>
-                        <td></td>
+                        <td width="60%" colSpan="2"></td>
+                        <td colSpan="2" width="40%">
+                            <Input value={searchTerm} onChange={searchHandler} id="searchInput" name="search" type="text" placeholder="Search"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td width="25%">Name</td>
+                        <td width="35%">Email</td>
+                        <td width="35%">Role</td>
+                        <td width="10%"></td>
                     </tr>
                 </thead>
                 <tbody>
-                    {props.users.map(user => (
+                    {searchResults.map(user => (
                         <tr key={user.id}>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.role}</td>
-                            <td>
+                            <td width="20%">{user.name}</td>
+                            <td width="35%">{user.email}</td>
+                            <td width="35%">{user.role}</td>
+                            <td width="10%">
                                 <ButtonGroup size="sm">
                                     <Button color="primary" onClick={() => props.editUser(user.id)}><FontAwesomeIcon icon={faEdit} /></Button>
                                     <Button color="danger" onClick={() => props.deleteUser(user.id)}><FontAwesomeIcon icon={faTrash} /></Button>
